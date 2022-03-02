cb1754205ee6399e6260546167eae8054ab2140a
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-03-01 23:07:53 -0600

    Create CSIAutoStrut.cs

---
+++ source/CSIAutoStrut.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using KSP;
+
+/// <summary>
+/// test
+/// </summary>
+namespace CSITools
+{
+    public class CSIAutoStrut : PartModule
+    {
+        [KSPField]
+        public string type = "disabled";
+
+        public override void OnStart(StartState state)
+        {
+            if (type == "ForceGrandparent")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.ForceGrandparent;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "ForceHeaviest")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.ForceHeaviest;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "Grandparent")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.Grandparent;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "Heaviest")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.Heaviest;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "Off")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.Off;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "Root")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.Root;
+                part.UpdateAutoStrut();
+                return;
+            }
+            if (type == "ForceRoot")
+            {
+                part.autoStrutMode = Part.AutoStrutMode.ForceRoot;
+                part.UpdateAutoStrut();
+                return;
+            }
+            else
+                Tools.LogFormatted("Input String Error, Check CFG settings");
+        }
+    }
+}
